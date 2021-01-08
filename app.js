b13c4e07fffdf6412f3edfb98dbed84e66934f63
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-07 23:30:32 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var inquirer = require("inquier");
+var connection = require('./connection');
+
+const viewOptions = [
+    "View Departments",
+    "View Roles",
+    "View Employees",
+    "Update Employee",
+    "Quit"
+];
---
+++ server.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  // Your port; if not 3306
+  port: 3306,
+  // Your username
+  user: "root",
+  // Your password
+  password: "root",
+  database: "employee_tracker_app_DB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection
