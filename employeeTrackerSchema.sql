22b211f474829eb549b9adcb115033266aa415c4
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-01-08 03:13:02 -0500

    Fixed issues with schema sql

--- employeeTrackerSchema.sql
+++ employeeTrackerSchema.sql
@@ -4,88 +4,71 @@ CREATE DATABASE employee_tracker_app_DB;
 
 USE employee_tracker_app_DB;
 
-CREATE TABLE Department(
-id INTEGER auto_increment not null,
-name VARCHAR(50) NOT NULL,
-PRIMARY KEY(id)
+CREATE TABLE department
+(id INT PRIMARY KEY AUTO_INCREMENT,
+department_name VARCHAR(30)
 );
 
-CREATE TABLE Role(
-id INTEGER auto_increment not null,
-title VARCHAR(50) NOT NULL,
-salary DECIMAL NOT NULL,
-department_id INTEGER NOT NULL,
-CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(id),
-PRIMARY KEY(id)
+CREATE TABLE roles
+(id INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL(10,2),
+department_id INT
 );
 
-CREATE TABLE Employee(
-id INTEGER auto_increment not null,
-first_name VARCHAR(50) NOT NULL,
-last_name VARCHAR(50) NOT NULL,
-role_id INTEGER NOT NULL,
-CONSTRAINT fk_role_id FOREIGN KEY (role_id) REFERENCES role(id),
-manager_id INTEGER ,
-CONSTRAINT fk_manager_id FOREIGN KEY (manager_id) REFERENCES employee(id),
-PRIMARY KEY(id)
+CREATE TABLE employee
+(id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT
 );
 
-SELECT * FROM Employee;
-SELECT * FROM Role;
-SELECT * FROM Department;
-
-INSERT INTO Department (name)
+INSERT INTO department (department_name)
 VALUES ("SALES");
-INSERT INTO Department (name)
+INSERT INTO department (department_name)
 VALUES ("Engineering");
-INSERT INTO Department (name)
+INSERT INTO department (department_name)
 VALUES ("Finace");
-INSERT INTO Department (name)
+INSERT INTO department (department_name)
 VALUES ("Legal");
-INSERT INTO Department (name)
+INSERT INTO department (department_name)
 VALUES ("Manager");
 
-SELECT * FROM Department;
-
-INSERT INTO Role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Head", 50000, 1);
-INSERT INTO Role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Sales Representative", 30000, 2);
-INSERT INTO Role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Senior Engineer", 55000, 3);
-INSERT INTO Role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Accountant", 45000, 4);
-INSERT INTO Role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Legal", 65000, 5);
-INSERT INTO Role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES ("Manager", 70000, 6);
 
-SELECT * FROM Role;
-
-
-INSERT INTO Role (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Jesus", "Emmanuel", 1);
-INSERT INTO Role (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Paul", "Abraham", 2);
-INSERT INTO Role (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Moses", "Levi", 3);
-INSERT INTO Role (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Joshua", "Caleb", 4);
-INSERT INTO Role (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("Samuel", "David", 5);
-INSERT INTO Role (first_name, last_name, role_id)
+INSERT INTO employee (first_name, last_name, role_id)
 VALUES ("John", "Peter", 6);
-INSERT INTO Role (first_name, last_name, role_id)
-VALUES ("Luke", "Matthew", 7);
-INSERT INTO Role (first_name, last_name, role_id)
-VALUES ("Anna", "Esther", 8);
-INSERT INTO Role (first_name, last_name, role_id)
-VALUES ("Mary", "Elizabeth", 9);
-INSERT INTO Role (first_name, last_name, role_id)
-VALUES ("Sarah", "Naomi", 10);
-INSERT INTO Role (first_name, last_name, role_id)
-VALUES ("Hannah", "Joanna", 11);
-INSERT INTO Role (first_name, last_name, role_id)
-VALUES ("Deborah", "Lois", 12);
-
-SELECT * FROM Employee;
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Luke", "Matthew", 1);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Anna", "Esther", 2);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Mary", "Elizabeth", 3);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Sarah", "Naomi", 4);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Hannah", "Joanna", 5);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Deborah", "Lois", 3);
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const express = require("express");
+const cTable = require("console.table");
 
-var connection = mysql.createConnection({
+const app = express();
+const PORT = 3301;
+
+const connection = mysql.createConnection({
   host: "localhost",
   // Your port; if not 3306
   port: 3306,
@@ -13,8 +18,459 @@ var connection = mysql.createConnection({
 });
 
 connection.connect(function (err) {
-    if (err) throw err;
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+
     console.log("connected as id " + connection.threadId);
+
+    startSearch();
 });
 
-module.exports = connection
+function startSearch() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "Add a department",
+                    value: "addDepartment",
+                },
+                {
+                    name: "Add a role",
+                    value: "addRole",
+                },
+                {
+                    name: "Add an employee",
+                    value: "addEmployee",
+                },
+                {
+                    name: "View all departments, roles, or employees",
+                    value: "view",
+                },
+                {
+                    name: "Update employee roles",
+                    value: "update",
+                },
+                {
+                    name: "Delete a department",
+                    value: "deleteDepartment",
+                },
+                {
+                    name: "Delete a role",
+                    value: "deleteRole",
+                },
+                {
+                    name: "Delete an employee",
+                    value: "deleteEmployee",
+                },
+                new inquirer.Separator(),
+                {
+                    name: "Exit The Employee Tracker",
+                    value: "exit",
+                },
+            ]
+        })
+
+        .then(function (answer) {
+            switch (answer.action) {
+                case "view":
+                    viewAll();
+                    break;
+
+                case "addDepartment":
+                    addDepartment();
+                    break;
+
+                case "addRole":
+                    addRole();
+                    break;
+
+                case "addEmployee":
+                    addEmployee();
+                    break;
+
+                case "update":
+                    updateRole();
+                    break;
+
+                case "deleteDepartment":
+                    deleteDepartment();
+                    break;
+
+                case "deleteRole":
+                    deleteRole();
+                    break;
+
+                case "deleteEmployee":
+                    deleteEmployee();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function deleteDepartment() {
+    // This displays a table of the departments with all the ID's
+    displayAllDepartments();
+
+    inquirer
+        .prompt({
+            name: "departmentId",
+            type: "input",
+            message: "Enter the ID of the department you want to delete",
+        })
+        .then((answer) => {
+            console.log("Deleting department...\n");
+
+            // Thhis deletes a department
+            connection.query(
+                "DELETE FROM department WHERE ?",
+                {
+                    id: answer.departmentId,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Department deleted!\n");
+
+                    // startSearch();
+                }
+            );
+
+            // This updates the table of roles which updates the id numbers once a department is deleted
+            connection.query(
+                "UPDATE roles SET ? WHERE ?",
+                [
+                    {
+                        department_id: "0",
+                    },
+                    {
+                        department_id: answer.departmentId,
+                    },
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(
+                        "Roles that were assigned to this department have been updated to '0' which signifies that they are now unassigned to a department.\n"
+                    );
+                }
+            );
+
+            startSearch();
+        });
+}
+function deleteRole() {
+    // This displays the department table to view all IDs
+    displayAllRoles();
+
+    inquirer
+        .prompt({
+            name: "roleId",
+            type: "input",
+            message: "Enter the ID of the role you want to delete",
+        })
+        .then((answer) => {
+            console.log("Deleting role...\n");
+
+            // This deletes a role from table
+            connection.query(
+                "DELETE FROM roles WHERE ?",
+                {
+                    id: answer.roleId,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Department deleted!\n");
+                }
+            );
+
+            // This updates the employee table which updates the id numbers once the role is deleted
+            connection.query(
+                "UPDATE employee SET ? WHERE ?",
+                [
+                    {
+                        role_id: "0",
+                    },
+                    {
+                        role_id: answer.roleId,
+                    },
+                ],
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(
+                        "Employees that were assigned to this role have been updated to '0' which signifies that they are now unassigned to a role.\n"
+                    );
+                }
+            );
+
+            startSearch();
+        });
+}
+
+function deleteEmployee() {
+    // This displays the department table to view all IDs
+    displayAllEmployees();
+
+    inquirer
+        .prompt({
+            name: "employeeId",
+            type: "input",
+            message: "Enter the ID of the employee you want to delete",
+        })
+        .then((answer) => {
+            console.log("Deleting employee...\n");
+            connection.query(
+                "DELETE FROM employee WHERE ?",
+                {
+                    id: answer.employeeId,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("Employee deleted!\n");
+                }
+            );
+
+            startSearch();
+        });
+}
+
+function updateRole() {
+    let employeeId;
+
+    // This displays the department table to view all IDs
+    displayAllEmployees();
+
+    inquirer
+        .prompt({
+            name: "employeeId",
+            type: "input",
+            message: "Enter the ID of the employee you want to update",
+        })
+        .then((answer) => {
+            employeeId = answer.employeeId;
+
+            // This displays the table of roles in order to select a role ID
+            displayAllRoles();
+
+            inquirer
+                .prompt({
+                    name: "roleId",
+                    type: "input",
+                    message: "Enter the role ID you want the user to have",
+                })
+                .then((answer) => {
+                    console.log("Updating employee role...\n");
+
+                    connection.query(
+                        "UPDATE employee SET ? WHERE ?",
+                        [
+                            {
+                                role_id: answer.roleId,
+                            },
+                            {
+                                id: employeeId,
+                            },
+                        ],
+                        function (err, res) {
+                            if (err) throw err;
+                            console.log("Employee role updated!\n");
+
+                            startSearch();
+                        }
+                    );
+                });
+        });
+}
+
+function addDepartment() {
+    inquirer
+        .prompt({
+            name: "department_name",
+            type: "input",
+            message: "What is the name of the department you want to add?",
+        })
+        .then((answer) => {
+            console.log("Adding a new department...\n");
+            connection.query(
+                `INSERT INTO department SET ?`,
+                {
+                    department_name: answer.department_name,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("New department added!\n");
+
+                    startSearch();
+                }
+            );
+        });
+}
+
+function addRole() {
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What is the name of the role that you would like to add?",
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary of this role?",
+                validate: function (value) {
+                    let valid = !isNaN(value);
+                    return valid || "Please enter a number";
+                },
+            },
+            {
+                name: "department_id",
+                type: "input",
+                message: "What is this role's department ID?",
+            },
+        ])
+        .then((answer) => {
+            console.log("Adding a new role...\n");
+            connection.query(
+                `INSERT INTO roles SET ?`,
+                {
+                    title: answer.title,
+                    salary: answer.salary,
+                    department_id: answer.department_id,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("New role added!\n");
+                    startSearch();
+                }
+            );
+        });
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                name: "firstName",
+                type: "input",
+                message: "What is the employee's first name?",
+            },
+            {
+                name: "lastName",
+                type: "input",
+                message: "What is the employee's last name?",
+            },
+            {
+                name: "roleId",
+                type: "input",
+                message: "What is this employee's role ID?",
+            },
+            {
+                name: "managerId",
+                type: "input",
+                message: "What is this employee's manager ID?",
+            },
+        ])
+        .then((answer) => {
+            console.log("Adding a new employee...\n");
+            connection.query(
+                `INSERT INTO employee SET ?`,
+                {
+                    first_name: answer.firstName,
+                    last_name: answer.lastName,
+                    role_id: answer.roleId,
+                    manager_id: answer.managerId,
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log("New role added!\n");
+
+                    startSearch();
+                }
+            );
+        });
+}
+
+function viewAll() {
+    inquirer
+        .prompt({
+            name: "table",
+            type: "list",
+            message:
+                "Would you like to view all departments, roles, or employees?",
+            choices: [
+                {
+                    name: "Departments",
+                    value: "department",
+                },
+                {
+                    name: "Roles",
+                    value: "roles",
+                },
+                {
+                    name: "Employees",
+                    value: "employee",
+                },
+            ],
+        })
+        .then(function (answer) {
+            console.log(`Selecting all from ${answer.table}...`);
+
+            switch (answer.table) {
+                case "department":
+                    displayAllDepartments();
+                    break;
+
+                case "roles":
+                    displayAllRoles();
+                    break;
+
+                case "employee":
+                    displayAllEmployees();
+                    break;
+            }
+
+            startSearch();
+        });
+}
+
+function displayAllEmployees() {
+    let query = "SELECT * FROM employee ";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        console.log("\n\n ** Full Employee list ** \n");
+        console.table(res);
+    });
+}
+
+function displayAllRoles() {
+    let query = "SELECT * FROM roles ";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        console.log("\n\n ** Full Role list ** \n");
+        console.table(res);
+    });
+}
+
+function displayAllDepartments() {
+    let query = "SELECT * FROM department ";
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+
+        console.log("\n\n ** Full Department list ** \n");
+        console.table(res);
+    });
+}
+
+// This starts the server
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
